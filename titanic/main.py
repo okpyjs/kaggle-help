14e7ba137a711d6ac8c4605d928a6c32b586ff53
Oleksandr <oleksandrko448@gmail.com>
2023-05-30 00:28:38 +0100

    first prediction with RandomForestClassifier

---
+++ titanic/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+train_data = pd.read_csv("train.csv")
+print(train_data.head())
+
+test_data = pd.read_csv("test.csv")
+print(test_data.head())
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({"PassengerId": test_data["PassengerId"], "Survived": predictions})
+output.to_csv("submission.csv", index=False)
+print(output)
